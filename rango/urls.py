f3d4845c25abde726cd97f88744c8323a0e1d650
Yufei Bai <2641131b@student.gla.ac.uk>
2022-01-28 00:22:24 +0000

    Chapter 3 completed

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     #如果匹配到'' 那么调用views.index, index是对view的convention
+    path('about/', views.about, name='about'),
+    #如果匹配到about/,那么调用about界面
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
+from cgitb import html
+from re import A
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
-
-    
+    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    #return HttpResponse("<a href='/rango/about/'>Rango says hey there partner!</a>")
+def about(request):
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    #return HttpResponse("<a href='/rango/'>Rango says here is the about page.</a>")
