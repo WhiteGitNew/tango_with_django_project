22eb498dbfdd958c5d4af9edc617c98fe9261c16
Yufei Bai <2641131b@student.gla.ac.uk>
2022-02-03 16:16:11 +0000

    chapter 4 completed

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,27 @@ from re import A
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import context
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
-    #return HttpResponse("<a href='/rango/about/'>Rango says hey there partner!</a>")
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    # 将模板变量名template variable 映射到python变量
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request,'rango/index.html',context=context_dict)
+    #render函数接收用户的request, 模板html文件和 context键值对字典
+
+    #return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    
+
+
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
-    #return HttpResponse("<a href='/rango/'>Rango says here is the about page.</a>")
+    #return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+
+    context_dict = {}
+    return render(request,'rango/about.html',context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     #让所有以rango/开头的URL交由rango来处理
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
